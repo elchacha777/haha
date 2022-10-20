bf3e099eb29c89476ea4b3ae809699551c95f05e
Atai <zver.cm@gmail.com>
2022-10-20 11:31:33 +0600

    askdklajksjdlklaksjdl

--- worker/post_reviews.py
+++ worker/post_reviews.py
@@ -53,15 +53,6 @@ class GoogleReviews:
         time.sleep(5)
         # check_cookie(self.driver)
         time.sleep(5)
-        cookie_accept_btn = cookie_accept_btn_exists(self.driver)
-
-        if cookie_accept_btn:
-            click_on_button(self.driver, cookie_accept_btn)
-            logger.info(f'cookie_accept_btn pressed {cookie_accept_btn}')
-            time.sleep(7)
-            self.get_review_page()
-        else:
-            logger.info(f'NO COOKIE BUTTON!!!')
 
     def check_verify(self):
         def login_btn_exists(driver):
@@ -89,37 +80,15 @@ class GoogleReviews:
             except:
                 logger.info('cant change to frame')
 
-    def find_button(self, by, element):
+    def find_button(self):
         for i in range(21):
             try:
-                button = self.driver.find_element(by, element)
+                button = self.driver.find_element(By.XPATH, '//*[@id="kCvOeb"]/div[1]/div[3]/div/div[2]/div/div[5]')
                 return button
                 break
             except:
                 logger.info('cant change to frame')
-    def find_buttons(self, by, element):
-        for i in range(21):
-            try:
-                button = self.driver.find_element(by, element)[-1]
-                return button
-                break
-            except:
-                logger.info('cant change to frame')
-                time.sleep(1)
 
-    def wait_element_for_click_1(self, element):
-        attempts = 20
-        while attempts:
-            try:
-                input_email = self.driver.find_element(element)
-                input_email.click()
-                logger.info('Click on button')
-                return True
-            except Exception as e:
-                logger.info(f'{e}')
-                attempts -= 1
-                time.sleep(1)
-        return
 
 
     def open_review(self):
@@ -158,15 +127,11 @@ class GoogleReviews:
         time.sleep(20)
         # button = self.driver.find_elements(By.CLASS_NAME, 's2xyy')
 
-        b = self.find_button(By.XPATH, '//*[@id="kCvOeb"]/div[1]/div[3]/div/div[2]/div/div[5]')
-        logger.info('found button star')
-        time.sleep(10)
-        self.wait_element_for_click_1(b)
-        # for i in range(21):
-        #     try:
-        #         b.click()
-        #     except:
-        #         self.driver.execute_script("var evt = document.createEvent('MouseEvents');" + "evt.initMouseEvent('click',true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0,null);" + "arguments[0].dispatchEvent(evt);", b)
+        b = self.find_button()
+        try:
+            b.click()
+        except:
+            self.driver.execute_script("var evt = document.createEvent('MouseEvents');" + "evt.initMouseEvent('click',true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0,null);" + "arguments[0].dispatchEvent(evt);", b)
 
 
 
@@ -174,14 +139,14 @@ class GoogleReviews:
 
 
         time.sleep(20)
-        # parent = self.driver.find_element(By.ID, 'ZRGZAf')
-        child = self.find_buttons(By.CLASS_NAME, 'VfPpkd-RLmnJb')
-        # child = parent.find_element(By.CLASS_NAME, 'VfPpkd-RLmnJb')
+
+        parent = self.driver.find_element(By.ID, 'ZRGZAf')
+        logger.info(f'{parent}')
+        child = parent.find_element(By.CLASS_NAME, 'VfPpkd-RLmnJb')
         logger.info(f'{child}')
 
-        time.sleep(10)
-        self.wait_element_for_click_1(child)
-        # self.driver.execute_script("var evt = document.createEvent('MouseEvents');" + "evt.initMouseEvent('click',true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0,null);" + "arguments[0].dispatchEvent(evt);", child)
+        time.sleep(5)
+        self.driver.execute_script("var evt = document.createEvent('MouseEvents');" + "evt.initMouseEvent('click',true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0,null);" + "arguments[0].dispatchEvent(evt);", child)
 
         logger.info('Review created')
 
